ec28ab60dd03ef1126ae0b7d031f4d7456782364
JoramM <jorammarkert@gmx.de>
2018-10-04 16:30:52 +0200

    Add documentation and shutdown button

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # A Raspberry Pi Radio
+
+This is a simple webapp written in Python 3 to control a RasperryPi, which is used as a radio. :)
+
+## Setup
+The LibVLC API is used to play music with the VLC Player.
+
+So you have to install the [VLC Player](https://www.videolan.org/vlc/#download). For Ubuntu use:
+```
+sudo apt-get insrall vlc
+```
+
+Navigate to project directory and install the Python requirements:
+```
+pip install -r requirements.txt
+```
+If you have Python 2.7 **and** Python 3 installed, use this command instead:
+```
+pip3 install -r requirements.txt
+```
+
+## Start
+```
+python main.py
+```
+If you have Python 2.7 **and** Python 3 installed, use this command instead:
+```
+python3 main.py
+```
+
+This will start the Flask webserver which is accessible over http://localhost:8080
+
+## Useful Links
+* [Jinja2 Documentation](http://jinja.pocoo.org/docs/)
+* [Flask Documentation](flask.pocoo.org/docs/)
+* [Bindings for the LibVLC public API](https://www.olivieraubert.net/vlc/python-ctypes/doc/)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, Markup, redirect, url_for
 import vlc
+import subprocess
 
 app = Flask(__name__)
 
@@ -7,7 +8,7 @@ menu = [
     ('/', 'Home')
 ]
 
-player = vlc.MediaPlayer("file:///home/joram/dev/raspberry/music/1 - Holy Water.mp3")
+player = vlc.MediaPlayer("file:///home/joram/dev/radio.py/music/1 - Holy Water.mp3")
 
 @app.route("/")
 def index():
@@ -16,7 +17,7 @@ def index():
 @app.route("/play")
 def play_music():
     player.play()
-    return redirect(url_for('index'))
+    return redirect(url_for('index'))   # stay on index-page
 
 @app.route("/pause")
 def pause_music():
@@ -26,6 +27,12 @@ def pause_music():
 @app.route("/stop")
 def stop_music():
     player.stop()
+    return redirect(url_for('index'))
+
+@app.route("/shutdown")
+def shutdown():
+    process = subprocess.Popen("sudo shutdown -h now", stdout=subprocess.PIPE)  # call a commandline function
+    output, error = process.communicate()
     return redirect(url_for('index'))
 
 if __name__ == "__main__":
